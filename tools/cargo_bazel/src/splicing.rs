0099b1b578333b6428833db79f80799f4006c305
Andre Brisco <andre.brisco@protonmail.com>
2021-10-14 06:32:21 -0700

    Slight cleanup and updated docs

--- tools/cargo_bazel/src/splicing.rs
+++ tools/cargo_bazel/src/splicing.rs
@@ -30,7 +30,6 @@ pub struct ExtraManifestInfo {
 }
 
 type DirectPackageManifest = BTreeMap<String, cargo_toml::DependencyDetail>;
-type ExtraManifestsDigest = Vec<ExtraManifestInfo>;
 
 #[derive(Debug, Default, Serialize, Deserialize)]
 #[serde(deny_unknown_fields)]
@@ -39,7 +38,7 @@ pub struct SplicingManifest {
     pub direct_packages: DirectPackageManifest,
 
     /// A collection of information required for reproducible "extra worksspace members".
-    pub extra_manifest_infos: ExtraManifestsDigest,
+    pub extra_manifest_infos: Vec<ExtraManifestInfo>,
 
     /// A mapping of manifest paths to the labels representing them
     pub manifests: BTreeMap<PathBuf, Label>,
@@ -172,19 +171,25 @@ impl WorkspaceMetadata {
     }
 }
 
+/// The core splicer implementation. Each style of Bazel workspace should be represented
+/// here and a splicing implementation defined.
 pub enum SplicerKind<'a> {
+    /// Splice a manifest which is represented by a Cargo workspace
     Workspace {
         path: &'a PathBuf,
         manifest: &'a Manifest,
         lockfile: &'a Option<PathBuf>,
         splicing_manifest: &'a SplicingManifest,
     },
+    /// Splice a manifest for a single package. This includes cases where
+    /// were defined directly in Bazel.
     Package {
         path: &'a PathBuf,
         manifest: &'a Manifest,
         lockfile: &'a Option<PathBuf>,
         splicing_manifest: &'a SplicingManifest,
     },
+    /// Splice a manifest from multiple disjoint Cargo manifests.
     MultiPackage {
         manifests: &'a HashMap<PathBuf, Manifest>,
         splicing_manifest: &'a SplicingManifest,
@@ -239,150 +244,195 @@ impl<'a> SplicerKind<'a> {
         }
     }
 
+    /// Performs splicing based on the current variant.
     pub fn splice(&self, workspace_dir: &Path, generator: &LockGenerator) -> Result<PathBuf> {
-        let get_extra_workspace_manifests =
-            |extra_manifests: &ExtraManifestsDigest| -> Result<HashMap<PathBuf, Manifest>> {
-                extra_manifests
-                    .iter()
-                    .map(|config| match read_manifest(&config.manifest) {
-                        Ok(manifest) => Ok((config.manifest.clone(), manifest)),
-                        Err(err) => Err(err),
-                    })
-                    .collect()
-            };
-
         match self {
             SplicerKind::Workspace {
                 path,
                 manifest,
                 lockfile,
                 splicing_manifest,
-            } => {
-                let mut manifest = (*manifest).clone();
-                let manifest_dir = path
-                    .parent()
-                    .expect("Every manifest should havee a parent directory");
-
-                let extra_workspace_manifests =
-                    get_extra_workspace_manifests(&splicing_manifest.extra_manifest_infos)?;
-
-                // Link the sources of the root manifest into the new workspace
-                symlink_roots(manifest_dir, workspace_dir)?;
-
-                // Add additional workspace members to the new manifest
-                let mut installations = Self::inject_workspace_members(
-                    &mut manifest,
-                    &extra_workspace_manifests,
-                    workspace_dir,
-                )?;
-
-                // Add any additional depeendencies to the root package
-                Self::inject_direct_packages(&mut manifest, &splicing_manifest.direct_packages)?;
-
-                let root_manifest_path = workspace_dir.join("Cargo.toml");
-                installations.insert(path, String::new());
-
-                // Write the generated metadata to the manifest
-                let workspace_metadata = WorkspaceMetadata::new(splicing_manifest, installations)?;
-                workspace_metadata.inject_into(&mut manifest)?;
-
-                // Write the root manifest
-                write_root_manifest(&root_manifest_path, manifest)?;
-
-                // Ensure a lockfile has been populated for the workspace
-                Self::generate_lockfile(generator, workspace_dir, lockfile)?;
-
-                Ok(root_manifest_path)
-            }
+            } => Self::splice_workspace(
+                workspace_dir,
+                generator,
+                path,
+                manifest,
+                lockfile,
+                splicing_manifest,
+            ),
             SplicerKind::Package {
                 path,
                 manifest,
                 lockfile,
                 splicing_manifest,
-            } => {
-                let manifest_dir = path
-                    .parent()
-                    .expect("Every manifest should havee a parent directory");
-
-                let extra_workspace_manifests =
-                    get_extra_workspace_manifests(&splicing_manifest.extra_manifest_infos)?;
-
-                // Link the sources of the root manifest into the new workspace
-                symlink_roots(manifest_dir, workspace_dir)?;
-
-                // Ensure the root package manifest has a populated `workspace` member
-                let mut manifest = (*manifest).clone();
-                if manifest.workspace.is_none() {
-                    manifest.workspace = default_cargo_workspace_manifest().workspace
-                }
-
-                // Add additional workspace members to the new manifest
-                let mut installations = Self::inject_workspace_members(
-                    &mut manifest,
-                    &extra_workspace_manifests,
-                    workspace_dir,
-                )?;
-
-                // Add any additional depeendencies to the root package
-                Self::inject_direct_packages(&mut manifest, &splicing_manifest.direct_packages)?;
-
-                let root_manifest_path = workspace_dir.join("Cargo.toml");
-                installations.insert(path, String::new());
-
-                // Write the generated metadata to the manifest
-                let workspace_metadata = WorkspaceMetadata::new(splicing_manifest, installations)?;
-                workspace_metadata.inject_into(&mut manifest)?;
-
-                // Write the root manifest
-                write_root_manifest(&root_manifest_path, manifest)?;
-
-                // Ensure a lockfile has been populated for the workspace
-                Self::generate_lockfile(generator, workspace_dir, lockfile)?;
-
-                Ok(root_manifest_path)
-            }
+            } => Self::splice_package(
+                workspace_dir,
+                generator,
+                path,
+                manifest,
+                lockfile,
+                splicing_manifest,
+            ),
             SplicerKind::MultiPackage {
                 manifests,
                 splicing_manifest,
-            } => {
-                let mut manifest = default_cargo_workspace_manifest();
-
-                let extra_workspace_manifests =
-                    get_extra_workspace_manifests(&splicing_manifest.extra_manifest_infos)?;
-
-                let manifests: HashMap<PathBuf, Manifest> = manifests
-                    .iter()
-                    .map(|(p, m)| (p.to_owned(), m.to_owned()))
-                    .collect();
-
-                let all_manifests = manifests
-                    .iter()
-                    .chain(extra_workspace_manifests.iter())
-                    .map(|(k, v)| (k.clone(), v.clone()))
-                    .collect();
-
-                let installations =
-                    Self::inject_workspace_members(&mut manifest, &all_manifests, workspace_dir)?;
-
-                // Write the generated metadata to the manifest
-                let workspace_metadata = WorkspaceMetadata::new(splicing_manifest, installations)?;
-                workspace_metadata.inject_into(&mut manifest)?;
-
-                // Add any additional depeendencies to the root package
-                Self::inject_direct_packages(&mut manifest, &splicing_manifest.direct_packages)?;
-
-                // Write the root manifest
-                let root_manifest_path = workspace_dir.join("Cargo.toml");
-                write_root_manifest(&root_manifest_path, manifest)?;
-
-                // Ensure a lockfile has been populated for the workspace
-                Self::generate_lockfile(generator, workspace_dir, &None)?;
-
-                Ok(root_manifest_path)
-            }
+            } => Self::splice_multi_package(workspace_dir, generator, manifests, splicing_manifest),
         }
     }
 
+    fn splice_workspace(
+        workspace_dir: &Path,
+        generator: &LockGenerator,
+        path: &&PathBuf,
+        manifest: &&Manifest,
+        lockfile: &&Option<PathBuf>,
+        splicing_manifest: &&SplicingManifest,
+    ) -> Result<PathBuf> {
+        let mut manifest = (*manifest).clone();
+        let manifest_dir = path
+            .parent()
+            .expect("Every manifest should havee a parent directory");
+
+        let extra_workspace_manifests =
+            Self::get_extra_workspace_manifests(&splicing_manifest.extra_manifest_infos)?;
+
+        // Link the sources of the root manifest into the new workspace
+        symlink_roots(manifest_dir, workspace_dir)?;
+
+        // Add additional workspace members to the new manifest
+        let mut installations = Self::inject_workspace_members(
+            &mut manifest,
+            &extra_workspace_manifests,
+            workspace_dir,
+        )?;
+
+        // Add any additional depeendencies to the root package
+        Self::inject_direct_packages(&mut manifest, &splicing_manifest.direct_packages)?;
+
+        let root_manifest_path = workspace_dir.join("Cargo.toml");
+        installations.insert(path, String::new());
+
+        // Write the generated metadata to the manifest
+        let workspace_metadata = WorkspaceMetadata::new(splicing_manifest, installations)?;
+        workspace_metadata.inject_into(&mut manifest)?;
+
+        // Write the root manifest
+        write_root_manifest(&root_manifest_path, manifest)?;
+
+        // Ensure a lockfile has been populated for the workspace
+        Self::generate_lockfile(generator, workspace_dir, lockfile)?;
+
+        Ok(root_manifest_path)
+    }
+
+    fn splice_package(
+        workspace_dir: &Path,
+        generator: &LockGenerator,
+        path: &&PathBuf,
+        manifest: &&Manifest,
+        lockfile: &&Option<PathBuf>,
+        splicing_manifest: &&SplicingManifest,
+    ) -> Result<PathBuf> {
+        let manifest_dir = path
+            .parent()
+            .expect("Every manifest should havee a parent directory");
+
+        let extra_workspace_manifests =
+            Self::get_extra_workspace_manifests(&splicing_manifest.extra_manifest_infos)?;
+
+        // Link the sources of the root manifest into the new workspace
+        symlink_roots(manifest_dir, workspace_dir)?;
+
+        // Ensure the root package manifest has a populated `workspace` member
+        let mut manifest = (*manifest).clone();
+        if manifest.workspace.is_none() {
+            manifest.workspace = default_cargo_workspace_manifest().workspace
+        }
+
+        // Add additional workspace members to the new manifest
+        let mut installations = Self::inject_workspace_members(
+            &mut manifest,
+            &extra_workspace_manifests,
+            workspace_dir,
+        )?;
+
+        // Add any additional depeendencies to the root package
+        Self::inject_direct_packages(&mut manifest, &splicing_manifest.direct_packages)?;
+
+        let root_manifest_path = workspace_dir.join("Cargo.toml");
+        installations.insert(path, String::new());
+
+        // Write the generated metadata to the manifest
+        let workspace_metadata = WorkspaceMetadata::new(splicing_manifest, installations)?;
+        workspace_metadata.inject_into(&mut manifest)?;
+
+        // Write the root manifest
+        write_root_manifest(&root_manifest_path, manifest)?;
+
+        // Ensure a lockfile has been populated for the workspace
+        Self::generate_lockfile(generator, workspace_dir, lockfile)?;
+
+        Ok(root_manifest_path)
+    }
+
+    fn splice_multi_package(
+        workspace_dir: &Path,
+        generator: &LockGenerator,
+        manifests: &&HashMap<PathBuf, Manifest>,
+        splicing_manifest: &&SplicingManifest,
+    ) -> Result<PathBuf> {
+        let mut manifest = default_cargo_workspace_manifest();
+
+        let extra_workspace_manifests =
+            Self::get_extra_workspace_manifests(&splicing_manifest.extra_manifest_infos)?;
+
+        let manifests: HashMap<PathBuf, Manifest> = manifests
+            .iter()
+            .map(|(p, m)| (p.to_owned(), m.to_owned()))
+            .collect();
+
+        let all_manifests = manifests
+            .iter()
+            .chain(extra_workspace_manifests.iter())
+            .map(|(k, v)| (k.clone(), v.clone()))
+            .collect();
+
+        let installations =
+            Self::inject_workspace_members(&mut manifest, &all_manifests, workspace_dir)?;
+
+        // Write the generated metadata to the manifest
+        let workspace_metadata = WorkspaceMetadata::new(splicing_manifest, installations)?;
+        workspace_metadata.inject_into(&mut manifest)?;
+
+        // Add any additional depeendencies to the root package
+        Self::inject_direct_packages(&mut manifest, &splicing_manifest.direct_packages)?;
+
+        // Write the root manifest
+        let root_manifest_path = workspace_dir.join("Cargo.toml");
+        write_root_manifest(&root_manifest_path, manifest)?;
+
+        // Ensure a lockfile has been populated for the workspace
+        Self::generate_lockfile(generator, workspace_dir, &None)?;
+
+        Ok(root_manifest_path)
+    }
+
+    /// Extract the set of extra workspace member manifests such that it matches
+    /// how other manifests are passed when creating a new [SplicerKind].
+    fn get_extra_workspace_manifests(
+        extra_manifests: &[ExtraManifestInfo],
+    ) -> Result<HashMap<PathBuf, Manifest>> {
+        extra_manifests
+            .iter()
+            .map(|config| match read_manifest(&config.manifest) {
+                Ok(manifest) => Ok((config.manifest.clone(), manifest)),
+                Err(err) => Err(err),
+            })
+            .collect()
+    }
+
+    /// Update the newly generated manifest to include additional packages as
+    /// Cargo workspace members.
     fn inject_workspace_members<'b>(
         root_manifest: &mut Manifest,
         manifests: &'b HashMap<PathBuf, Manifest>,
@@ -470,7 +520,7 @@ impl<'a> SplicerKind<'a> {
             return Ok(());
         }
 
-        // Remove the file so it's not overwitten if it happens to be a symlink
+        // Remove the file so it's not overwitten if it happens to be a symlink.
         if root_lockfile_path.exists() {
             fs::remove_file(&root_lockfile_path)?;
         }
@@ -588,7 +638,7 @@ mod test {
         manifest
     }
 
-    fn mock_extra_manifest_digest(cache_dir: &Path) -> ExtraManifestsDigest {
+    fn mock_extra_manifest_digest(cache_dir: &Path) -> Vec<ExtraManifestInfo> {
         vec![{
             let manifest_path = cache_dir.join("extra_pkg").join("Cargo.toml");
             mock_cargo_toml(&manifest_path, "extra_pkg");
