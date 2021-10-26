12cf9719d42d308ef16c6934276cd829b7ee16d5
Andre Brisco <andre.brisco@protonmail.com>
2021-10-25 18:22:16 -0700

    Temporarily disable examples test in CI for windows

--- tests/examples_test.sh
+++ tests/examples_test.sh
@@ -9,8 +9,16 @@ else
     EXAMPLES_DIR="${BUILD_WORKSPACE_DIRECTORY}/examples"
 fi
 
-export CARGO_BAZEL_GENERATOR_URL="file://$(pwd)/${CARGO_BAZEL_BIN}"
-export CARGO_BAZEL_GENERATOR_SHA256="$(shasum -a 256 "$(pwd)/${CARGO_BAZEL_BIN}" | awk '{ print $1 }')"
+if [[ "${OS:-}" == "Windows"* ]]; then
+    # TODO: Export sha256 on windows
+    export CARGO_BAZEL_GENERATOR_URL="file://$(echo ${PWD} | sed 's|^/c/|/|')/${CARGO_BAZEL_BIN}"
+    # TODO: Windows reads `//...` as `/...` for unknown reasons...
+    TARGETS='///...'
+else
+    export CARGO_BAZEL_GENERATOR_URL="file://$(pwd)/${CARGO_BAZEL_BIN}"
+    export CARGO_BAZEL_GENERATOR_SHA256="$(shasum -a 256 "$(pwd)/${CARGO_BAZEL_BIN}" | awk '{ print $1 }')"
+    TARGETS='//...'
+fi
 
 if [[ -z "${BAZEL_STARTUP_FLAGS:-}" ]]; then
     export BAZEL_STARTUP_FLAGS=("")
@@ -23,11 +31,11 @@ fi
 set -x
 pushd "${EXAMPLES_DIR}" &>/dev/null
 
-bazel ${BAZEL_STARTUP_FLAGS[@]} ${EXAMPLES_BAZEL_STARTUP_FLAGS[@]} build //...
-bazel ${BAZEL_STARTUP_FLAGS[@]} ${EXAMPLES_BAZEL_STARTUP_FLAGS[@]} test //...
+bazel ${BAZEL_STARTUP_FLAGS[@]} ${EXAMPLES_BAZEL_STARTUP_FLAGS[@]} build ${TARGETS}
+bazel ${BAZEL_STARTUP_FLAGS[@]} ${EXAMPLES_BAZEL_STARTUP_FLAGS[@]} test ${TARGETS}
 
 export CARGO_BAZEL_REPIN=1
-bazel ${BAZEL_STARTUP_FLAGS[@]} ${EXAMPLES_BAZEL_STARTUP_FLAGS[@]} build //...
-bazel ${BAZEL_STARTUP_FLAGS[@]} ${EXAMPLES_BAZEL_STARTUP_FLAGS[@]} test //...
+bazel ${BAZEL_STARTUP_FLAGS[@]} ${EXAMPLES_BAZEL_STARTUP_FLAGS[@]} build ${TARGETS}
+bazel ${BAZEL_STARTUP_FLAGS[@]} ${EXAMPLES_BAZEL_STARTUP_FLAGS[@]} test ${TARGETS}
 
 popd &>/dev/null
