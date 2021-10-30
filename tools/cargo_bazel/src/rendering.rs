c1c4ffac88523ae83c0e688585d4c1737b3c4758
Andre Brisco <91817010+abrisco@users.noreply.github.com>
2021-10-30 11:39:09 -0700

    Moved `render_utils` into `rendering` (#48)

--- tools/cargo_bazel/src/rendering.rs
+++ tools/cargo_bazel/src/rendering.rs
@@ -11,7 +11,6 @@ use anyhow::{Context as AnyhowContext, Result};
 use crate::config::RenderConfig;
 use crate::context::Context;
 use crate::rendering::template_engine::TemplateEngine;
-use crate::utils::render_utils::render_build_file_template;
 
 pub struct Renderer {
     config: RenderConfig,
@@ -89,4 +88,52 @@ pub fn write_outputs(
     }
 
     Ok(())
+}
+
+/// Render the Bazel label of a crate
+pub fn render_crate_bazel_label(
+    template: &str,
+    repository_name: &str,
+    name: &str,
+    version: &str,
+    target: &str,
+) -> String {
+    template
+        .replace("{repository}", repository_name)
+        .replace("{name}", name)
+        .replace("{version}", version)
+        .replace("{target}", target)
+}
+
+/// Render the Bazel label of a crate
+pub fn render_crate_bazel_repository(
+    template: &str,
+    repository_name: &str,
+    name: &str,
+    version: &str,
+) -> String {
+    template
+        .replace("{repository}", repository_name)
+        .replace("{name}", name)
+        .replace("{version}", version)
+}
+
+/// Render the Bazel label of a crate
+pub fn render_crate_build_file(template: &str, name: &str, version: &str) -> String {
+    template
+        .replace("{name}", name)
+        .replace("{version}", version)
+}
+
+/// Render the Bazel label of a platform triple
+pub fn render_platform_constraint_label(template: &str, triple: &str) -> String {
+    template.replace("{triple}", triple)
+}
+
+pub fn render_build_file_template(template: &str, name: &str, version: &str) -> PathBuf {
+    PathBuf::from(
+        template
+            .replace("{name}", name)
+            .replace("{version}", version),
+    )
 }
