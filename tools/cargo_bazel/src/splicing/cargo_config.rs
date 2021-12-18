c8d0d41d06df2ad4a41991e650f208518bfe49d0
Andre Brisco <andre.brisco@protonmail.com>
2021-12-18 11:16:28 -0800

    Updated reproducibility of Cargo config deserialization.

--- tools/cargo_bazel/src/splicing/cargo_config.rs
+++ tools/cargo_bazel/src/splicing/cargo_config.rs
@@ -1,6 +1,6 @@
 //! Tools for parsing [Cargo configuration](https://doc.rust-lang.org/cargo/reference/config.html) files
 
-use std::collections::HashMap;
+use std::collections::BTreeMap;
 use std::fs;
 use std::path::Path;
 use std::str::FromStr;
@@ -10,7 +10,7 @@ use serde::Deserialize;
 
 /// The [`[registry]`](https://doc.rust-lang.org/cargo/reference/config.html#registry)
 /// table controls the default registry used when one is not specified.
-#[derive(Debug, Deserialize)]
+#[derive(Debug, Deserialize, PartialEq, Eq)]
 pub struct Registry {
     /// name of the default registry
     pub default: String,
@@ -21,7 +21,7 @@ pub struct Registry {
 
 /// The [`[source]`](https://doc.rust-lang.org/cargo/reference/config.html#source)
 /// table defines the registry sources available.
-#[derive(Debug, Deserialize)]
+#[derive(Debug, Deserialize, PartialEq, Eq)]
 pub struct Source {
     /// replace this source with the given named source
     #[serde(rename = "replace-with")]
@@ -37,7 +37,7 @@ fn default_registry_url() -> String {
     "https://github.com/rust-lang/crates.io-index".to_owned()
 }
 
-#[derive(Debug, Deserialize)]
+#[derive(Debug, Deserialize, PartialEq, Eq)]
 /// registries other than crates.io
 pub struct AdditionalRegistry {
     /// URL of the registry index
@@ -51,23 +51,23 @@ pub struct AdditionalRegistry {
 /// is required for parsing registry information.
 /// See [cargo docs](https://doc.rust-lang.org/cargo/reference/config.html#configuration-format)
 /// for more details.
-#[derive(Debug, Deserialize)]
+#[derive(Debug, Deserialize, PartialEq, Eq)]
 pub struct CargoConfig {
     /// registries other than crates.io
     #[serde(default = "default_registries")]
-    pub registries: HashMap<String, AdditionalRegistry>,
+    pub registries: BTreeMap<String, AdditionalRegistry>,
 
     #[serde(default = "default_registry")]
     pub registry: Registry,
 
     /// source definition and replacement
-    #[serde(default = "HashMap::new")]
-    pub source: HashMap<String, Source>,
+    #[serde(default = "BTreeMap::new")]
+    pub source: BTreeMap<String, Source>,
 }
 
 /// Each Cargo config is expected to have a default `crates-io` registry.
-fn default_registries() -> HashMap<String, AdditionalRegistry> {
-    let mut registries = HashMap::new();
+fn default_registries() -> BTreeMap<String, AdditionalRegistry> {
+    let mut registries = BTreeMap::new();
     registries.insert(
         "crates-io".to_owned(),
         AdditionalRegistry {
@@ -115,7 +115,7 @@ impl FromStr for CargoConfig {
 
 impl CargoConfig {
     /// Load a Cargo conig from a path to a file on disk.
-    pub fn from_path(path: &Path) -> Result<Self> {
+    pub fn try_from_path(path: &Path) -> Result<Self> {
         let content = fs::read_to_string(path)?;
         Self::from_str(&content)
     }
@@ -123,5 +123,65 @@ impl CargoConfig {
     /// Look up a reigstry [Source] by it's url.
     pub fn get_source_from_url(&self, url: &str) -> Option<&Source> {
         self.source.values().find(|v| v.registry == url)
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    use std::fs;
+
+    #[test]
+    fn registry_settings() {
+        let temp_dir = tempfile::tempdir().unwrap();
+        let config = temp_dir.as_ref().join("config.toml");
+
+        fs::write(&config, textwrap::dedent(
+            r##"
+                # Makes artifactory the default registry and saves passing --registry parameter
+                [registry]
+                default = "art-crates-remote"
+                
+                [registries]
+                # Remote repository proxy in Artifactory (read-only)
+                art-crates-remote = { index = "https://artprod.mycompany/artifactory/git/cargo-remote.git" }
+                
+                # Optional, use with --registry to publish to crates.io
+                crates-io = { index = "https://github.com/rust-lang/crates.io-index" }
+
+                [net]
+                git-fetch-with-cli = true
+            "##,
+        )).unwrap();
+
+        let config = CargoConfig::try_from_path(&config).unwrap();
+        assert_eq!(
+            config,
+            CargoConfig {
+                registries: BTreeMap::from([
+                    (
+                        "art-crates-remote".to_owned(),
+                        AdditionalRegistry {
+                            index: "https://artprod.mycompany/artifactory/git/cargo-remote.git"
+                                .to_owned(),
+                            token: None,
+                        },
+                    ),
+                    (
+                        "crates-io".to_owned(),
+                        AdditionalRegistry {
+                            index: "https://github.com/rust-lang/crates.io-index".to_owned(),
+                            token: None,
+                        },
+                    ),
+                ]),
+                registry: Registry {
+                    default: "art-crates-remote".to_owned(),
+                    token: None,
+                },
+                source: BTreeMap::new(),
+            },
+        )
     }
 }
